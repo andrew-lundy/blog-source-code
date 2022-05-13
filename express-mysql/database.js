c6d35e7ebca2517bf03a6bf7a88fde6b5e415a8b
Andrew Lundy <andrewlundy@icloud.com>
2022-05-12 19:04:08 -0500

    Reset VS Code

--- express-mysql/database.js
+++ express-mysql/database.js
@@ -1,17 +1,24 @@
-var mysql = require('mysql2');
 require('dotenv').config();
 
-var connection = mysql.createConnection({
+let mysql = require('mysql2');
+let connection = mysql.createConnection({
     host     : process.env.DATABASE_HOST,
     user     : process.env.DATABASE_USER, 
     password : process.env.DATABASE_PASSWORD, 
     database : process.env.DATABASE_NAME
 });
 
-connection.connect((err => {
-  if(err) throw err;
-  console.log('MySQL Connected');
-}));
+
+// function connectToDatabase() {
+//   connection.connect((err => {
+//     if(err) throw err;
+//     console.log('MySQL Connected');
+//   }));
+  
+//   connection.end();
+// }
+
+
 
 exports.databaseConnection = connection;
 
--- express-mysql/routes/inventory.js
+++ express-mysql/routes/inventory.js
@@ -1,16 +1,37 @@
+require('dotenv').config();
+
 let express = require('express');
 let router = express.Router();
-
-let connection = require('../database').databaseConnection;
+let mysql = require('mysql2');
 
 router.get('/get-inventory', (req, res) => {
     let sql = 'SELECT * FROM inventory';
 
+    var connection = mysql.createConnection({
+        host     : process.env.DATABASE_HOST,
+        user     : process.env.DATABASE_USER, 
+        password : process.env.DATABASE_PASSWORD, 
+        database : process.env.DATABASE_NAME
+    });
+
     connection.query(sql, (err, result) => {
-    if(err) throw err;
-    console.log(result);
-    res.send('Inventory received via Router');
-  });
+        if(err) throw err;
+        console.log("My SQL Connected via a new open connection.");
+        res.send('Inventory received via Router');
+    });
+
+    connection.end((err => {
+        if(err) throw err;
+        console.log("Connection closed.");
+    }));
 });
 
 module.exports = router;
+
+
+
+
+    // connection.connect((err => {
+    //     if(err) throw err;
+    //     console.log("My SQL Connected via a new open connection.");
+    // }));
