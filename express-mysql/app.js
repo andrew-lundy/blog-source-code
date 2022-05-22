a0e172093382621ce735dee7d0aa6490037e8d49
Andrew Lundy <andrewlundy@icloud.com>
2022-05-22 15:50:29 -0500

    Remove unneeded info in app.js

--- express-mysql/app.js
+++ express-mysql/app.js
@@ -4,18 +4,5 @@ let app = express();
 let inventory = require('./routes/inventory');
 app.use('/inventory', inventory);
 
- // 1. Require the connection to the database.
- var connection = require('./database').databaseConnection;
-    
- // 2. Make the GET request.
- app.get('/inventory', (req, res) => {
-     let sql = 'SELECT * FROM inventory';
-     
-     connection.query(sql, (err, result) => {
-         if(err) throw  err;
-         console.log(result);
-         res.send('Inventory received');
-     });
- });
 
 module.exports = app;
