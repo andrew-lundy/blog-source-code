17bcde65c3ccef556dbe6c4dbc5e10a5009abc18
Andrew Lundy <andrewlundy@icloud.com>
2022-11-06 15:27:14 -0600

    Add example dashboards

--- bubbling/app.js
+++ bubbling/app.js
@@ -1,12 +1,18 @@
+// Menu Stuff
 const menuButton = document.getElementById('top-nav-bar__menu-button');
 const sideNavMenu = document.getElementById('side-nav-menu');
 const closeButton = document.getElementById('side-nav-menu__close-button');
-const servicesMenuItem = document.getElementById('side-nav-menu__sub-menu__dashboard');
-const servicesMenuContent = document.getElementById('side-nav-menu__sub-menu__dashboard-content');
+const dashboardMenuItem = document.getElementById('side-nav-menu__sub-menu__dashboard');
+const dashboardMenuContent = document.getElementById('side-nav-menu__sub-menu__dashboard-content');
 
-sideNavMenu.addEventListener('click', function (event) {
-    sideNavMenu.style.width = '0px';
-});
+// Dashboards
+const dashboardOneButton = document.getElementById('side-nav-menu__sub-menu__dashboard-content__dashboard-one');
+const dashboardTwoButton = document.getElementById('side-nav-menu__sub-menu__dashboard-content__dashboard-two');
+const dashboardThreeButton = document.getElementById('side-nav-menu__sub-menu__dashboard-content__dashboard-three');
+const dashboardContent = document.getElementById('dashboard-container__content');
+
+const dashboards = [dashboardOneButton, dashboardTwoButton, dashboardThreeButton];
+
 
 menuButton.addEventListener('click', function(event) {
     sideNavMenu.style.width = '250px';
@@ -14,15 +20,32 @@ menuButton.addEventListener('click', function(event) {
 
 closeButton.addEventListener('click', function(event) {
     sideNavMenu.style.width = '0px';
-})
-
-servicesMenuItem.addEventListener('click', function (event) {
-
-    if (servicesMenuContent.style.display === 'flex') {
-        servicesMenuContent.style.display = 'none';
-    } else {
-        servicesMenuContent.style.display = 'flex';
-        servicesMenuContent.style.flexDirection = 'column';
-    }
-
 });
+
+sideNavMenu.addEventListener('click', function (event) {
+    sideNavMenu.style.width = '0px';
+});
+
+dashboardMenuItem.addEventListener('click', function (event) {
+    event.stopPropagation();
+    if (dashboardMenuContent.style.display === 'flex') {
+        dashboardMenuContent.style.display = 'none';
+    } else {
+        dashboardMenuContent.style.display = 'flex';
+        dashboardMenuContent.style.flexDirection = 'column';
+    }
+});
+
+function showDashboard(event) {
+    const html = event.target.innerHTML;
+
+    dashboardContent.innerHTML = html;
+}
+
+for (const dashboard of dashboards) {
+    dashboard.addEventListener('click', function (event) {
+        showDashboard(event);
+        console.log(event);
+    });
+}
+
